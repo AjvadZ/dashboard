c33dca30ddd4b2f80db4bc2ebb28d54c9b7ff703
Ajvad <reachajvad@gmail.com>
2025-10-03 15:43:26 +0530

    Initial project setup

--- app/api/test/route.ts
+++
@@ -1,7 +0,0 @@
-import {sql} from '@/app/lib/data'
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const result = await sql`SELECT NOW();`;
-  return NextResponse.json(result);
-}
